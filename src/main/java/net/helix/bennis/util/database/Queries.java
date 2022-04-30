3d445ab8c830de111c9dc87d1a87a9cb5ede523d
James <wtechboy18@gmail.com>
2022-04-29 22:25:00 -0400

    Code & resource cleanup.

--- src/main/java/net/helix/bennis/util/database/Queries.java
+++ src/main/java/net/helix/bennis/util/database/Queries.java
@@ -1,7 +1,7 @@
 package net.helix.bennis.util.database;
 
 public class Queries {
-    private static const String GET_GIFT_LOCATIONS = "SELECT X_COORD, Y_COORD, Z_COORD FROM dbo.GiftBlocks";
+    private static final String GET_GIFT_LOCATIONS = "SELECT X_COORD, Y_COORD, Z_COORD FROM dbo.GiftBlocks";
 
     public static String BuildGetGiftLocationsQuery() {
         return GET_GIFT_LOCATIONS;
